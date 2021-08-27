14c2f67fe09ff571e240073b305cfb341860913b
Erhan Kabuk <erhan.kabuk@bennusoft.com>
2021-08-27 17:46:41 +0300

    BilgeKafe.Data  temeli oluşturuldu.

---
+++ BilgeKafe.Data/SiparisDetay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyati { get; set; }
+        public int Adet { get; set; }
+        public string TutarTL
+        {
+            get
+            {
+                return $"{Tutar():n2}₺";
+            }
+        }
+        public decimal Tutar()
+        {
+            return Adet * BirimFiyati;
+        }
+
+    }
+}
