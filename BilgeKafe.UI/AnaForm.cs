272e85674f2538ca382eff0437459abc55e2c924
Erhan Kabuk <erhan.kabuk@bennusoft.com>
2021-09-01 18:16:45 +0300

    Serialization & Deserialization

--- BilgeKafe.UI/AnaForm.cs
+++ BilgeKafe.UI/AnaForm.cs
@@ -1,10 +1,12 @@
 ﻿using BilgeKafe.Data;
 using BilgeKafe.UI.Properties;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,24 +15,36 @@ using System.Windows.Forms;
 namespace BilgeKafe.UI
 {
     public partial class AnaForm : Form
-    {   KafeVeri db= new KafeVeri();
+    {
+        KafeVeri db = new KafeVeri();
         public AnaForm()
         {
-            OrnekUrunleriOlustur();
+            VerileriOku();
+            // OrnekUrunleriOlustur();
             InitializeComponent();
             MasalariOlustur();
         }
 
+        private void VerileriOku()
+        {
+            try
+            {
+                string json = File.ReadAllText("veri.json");//Diskten okuma
+                db = JsonConvert.DeserializeObject<KafeVeri>(json);//Deserialization
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
         private void OrnekUrunleriOlustur()
         {
-            
             db.Urunler.Add(new Urun() { UrunAd = "Kola", BirimFiyat = 5.99m });
             db.Urunler.Add(new Urun() { UrunAd = "Ayran", BirimFiyat = 4.50m });
             db.Urunler.Add(new Urun() { UrunAd = "Çay", BirimFiyat = 2.99m });
-            
-
-
-
         }
 
         private void MasalariOlustur()
@@ -39,13 +53,13 @@ namespace BilgeKafe.UI
             imageList.Images.Add("bos", Resources.bos);
             imageList.Images.Add("dolu", Resources.dolu);
             imageList.ImageSize = new Size(64, 64);
-            lvmMasalar.LargeImageList = imageList; 
-            
+            lvmMasalar.LargeImageList = imageList;
+
             for (int i = 1; i <= db.MasaAdet; i++)
             {
                 ListViewItem lvi = new ListViewItem($"Masa {i}");
                 lvi.Tag = i;
-                lvi.ImageKey = "bos";
+                lvi.ImageKey = db.AktifSiparisler.Any(s => s.MasaNo == i) ? "dolu" : "bos";
                 lvmMasalar.Items.Add(lvi);
             }
         }
@@ -53,25 +67,25 @@ namespace BilgeKafe.UI
         private void lvmMasalar_DoubleClick(object sender, EventArgs e)
         {
             ListViewItem lvi = lvmMasalar.SelectedItems[0];
-            lvi.ImageKey = "dolu";
             int masaNo = (int)lvi.Tag;
+            lvi.ImageKey = "dolu";
             //Tıklanan masaya ait vaesrsa siparişi bul
             Siparis siparis = db.AktifSiparisler.FirstOrDefault(x => x.MasaNo == masaNo);
             //siparis oluşturulmadıysa o masanın
             if (siparis == null)
             {
-                siparis = new Siparis() { MasaNo = masaNo};
+                siparis = new Siparis() { MasaNo = masaNo };
                 db.AktifSiparisler.Add(siparis);
             }
-            
+
 
             SiparisForm frmSiparis = new SiparisForm(db, siparis);
             frmSiparis.ShowDialog();
-            if (siparis.Durum!=SiparisDurum.Aktif)
+            if (siparis.Durum != SiparisDurum.Aktif)
             {
                 lvi.ImageKey = "bos";//15:14 saatinde anlatıldı oradan bak
             }
-           
+
         }
 
         private void tsmiUrunler_Click(object sender, EventArgs e)
@@ -83,6 +97,12 @@ namespace BilgeKafe.UI
         {
             new GecmisSiparislerForm(db).ShowDialog();
 
+        }
+
+        private void AnaForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(db);//Serialization
+            File.WriteAllText("veri.json", json);//diske yazılması.
         }
     }
 }
